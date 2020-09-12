ef5413892c977bf36b3e90e8dc1a0ee55816eef1
abdellatif <you@example.com>
2020-09-12 18:08:02 +0100

    'fix themes'

---
+++ lib/theme.dart
@@ -0,0 +1,45 @@
+import 'package:donateplasma/constants/color_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screen_scaler/flutter_screen_scaler.dart';
+import 'widgets/constants.dart';
+
+
+
+ThemeData buildThemeData() {
+
+  ScreenScaler scale = ScreenScaler();
+  OutlineInputBorder inputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(30),
+    borderSide: const BorderSide(color: Colors.transparent),
+  );
+
+  return ThemeData(
+
+    scaffoldBackgroundColor: backgroundColor,
+    primaryColor: kPrimaryColor,
+    accentColor: kAccentColor,
+    dividerTheme: const DividerThemeData(thickness: 1, space: 30),
+
+    fontFamily: 'Rubik',
+    textTheme: TextTheme(
+      button: TextStyle(fontSize: scale.getFullScreen(7), fontWeight: FontWeight.w400),
+    ),
+
+    buttonTheme: ButtonThemeData(
+      padding: EdgeInsets.symmetric(vertical: scale.getFullScreen(6.5)),
+      shape: const StadiumBorder(),
+      textTheme: ButtonTextTheme.accent,
+    ),
+
+    cursorColor: primaryColor,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: accentColor,
+      focusedBorder: inputBorder,
+      enabledBorder: inputBorder,
+      contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: scale.getFullScreen(7)),
+    ),
+  );
+}
+
+
